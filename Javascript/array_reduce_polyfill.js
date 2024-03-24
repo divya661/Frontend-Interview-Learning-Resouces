54acacecbcc6d22b66a3755ea137ff003709265f
DIVYA CHAUHAN <divyac091@gmail.com>
2024-03-24 11:20:15 +0000

    reduce polyfill

---
+++ Javascript/array_reduce_polyfill.js
@@ -0,0 +1,16 @@
+Array.prototype.myReduce = function (callback, initialValue) {
+    let accumulator = initialValue;
+  
+    for (let i = 0; i < this.length; i++) {
+      if (accumulator) {
+        accumulator = callback.call(undefined, accumulator, this[i], i, this);
+      } else {
+        accumulator = this[i];
+      }
+    }
+  
+    return accumulator;
+  };
+  const arr = [1, 2, 3, 4];
+  console.log(arr.myReduce((total, elem) => total + elem));
+    
